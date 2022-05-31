d47327aa3d086d9d4616787265b0bb47e454e7f2
Ashmitathakur <asmee2056@gmail.com>
2022-05-31 20:35:09 +0545

    Profile for user done

--- OwnMe/accounts/models.py
+++ OwnMe/accounts/models.py
@@ -4,7 +4,26 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 from django.urls import reverse
 
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user   
     
+     
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=255, null=True,
                                   verbose_name=_("Firstname"))
