b10b43c31262ffa1cf85e465bee9ba5d422b293a
Shaurya <sahishaurya313@gmail.com>
2022-06-25 17:02:52 +0545

    Time and date fixed.

--- OwnMe/project/settings.py
+++ OwnMe/project/settings.py
@@ -134,6 +134,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
+TIME_ZONE = 'Europe/Berlin'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
