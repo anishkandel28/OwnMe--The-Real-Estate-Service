24dba9f8f7895b5ec4d03be04b25e99d2508f43b
Anish Shree Kaandel <anishkandel100@gmail.com>
2022-07-15 10:38:42 +0545

    Social Account Provider

--- OwnMe/project/settings.py
+++ OwnMe/project/settings.py
@@ -45,8 +45,24 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',  # for google auth
     
+
 ]
+
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
